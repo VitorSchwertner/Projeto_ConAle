9d306014b8d5be05a0e130d9c0c74892e962a4c7
arthur <arthur.dorst@universo.univates.br>
2026-09-16 21:21:32 -0300

    Criação de pastas e classes

---
+++ src/main/java/com/conAle/projeto/model/Noticia.java
@@ -0,0 +1,28 @@
+package com.conAle.projeto.model;
+
+public class Noticia
+{
+    private int id;
+    private String titulo;
+    private String subTitulo;
+
+    public String getTitulo()
+    {
+        return titulo;
+    }
+
+    public void setTitulo( String titulo )
+    {
+        this.titulo = titulo;
+    }
+
+    public String getSubTitulo()
+    {
+        return subTitulo;
+    }
+
+    public void setSubTitulo( String subTitulo )
+    {
+        this.subTitulo = subTitulo;
+    }
+}
---
+++ src/main/java/com/conAle/projeto/model/Rodape.java
@@ -0,0 +1,12 @@
+package com.conAle.projeto.model;
+
+public class Rodape
+{
+    private String telefone;
+    private String instagram;
+    private String email;
+    private String link;
+    private String diasSemana;
+    private String horasTrabalhadas;
+    private String local;
+}
